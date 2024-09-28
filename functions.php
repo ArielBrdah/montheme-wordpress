1cd90aa5269955baeea4699a40d2a2465398b0b1
leira <arielberdah@outlook.com>
2024-09-28 21:43:02 +0300

    add style for agence and columns

--- functions.php
+++ functions.php
@@ -134,3 +134,39 @@ require_once('options/agence.php');
 
 SponsoMetabox::register();
 AgenceMenuPage::register();
+
+add_filter('manage_bien_posts_columns', function ($columns){ 
+    return [
+        'cb' => $columns['cb'],
+        'thumbnail' => 'Miniature',
+        'title' => $columns['title'],
+        'date' => $columns['date'],
+    ];
+});
+
+add_filter('manage_bien_posts_custom_column', function ($column, $postId) {
+    if($column === 'thumbnail') the_post_thumbnail('thumbnail', $postId);
+}, 10, 2);
+
+
+add_action('admin_enqueue_scripts', function() {
+    wp_enqueue_style('admin_montheme', get_template_directory_uri().'/assets/admin.css');
+});
+
+
+add_filter('manage_post_posts_columns', function ($columns){ 
+    return [
+        ...$columns,
+        'sponso' => 'sponso' 
+   ];
+});
+
+add_filter('manage_post_posts_custom_column', function ($column, $postId) {
+    if($column === 'sponso') {
+        $checked = 'yes';
+        if (!get_post_meta($postId, SponsoMetabox::META_KEY, true)) $checked = 'no';
+        else $checked = 'yes';
+        
+        echo "<div class='bullet bullet-".$checked."'></div>";
+    }
+}, 10, 2);
